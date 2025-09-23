76a94643fb4b4e50a6ae445acaf0dfcb8fb88914
Alex Gittings <agitting96@gmail.com>
2025-09-23 10:01:28 +0100

    Add configuration files and enhance task definitions for linting and schema loading
    - Create .graphqlrc.yml for GraphQL schema configuration.
    - Add .yamllint.yml for YAML linting rules.
    - Update pyproject.toml.jinja to include mypy and yamllint dependencies.
    - Enhance tasks.py with new tasks for loading schemas and running linters (yamllint, mypy, ruff).

--- tasks.py
+++ tasks.py
@@ -6,6 +6,8 @@ from invoke import Context, task
 
 # If no version is indicated, we will take the latest
 VERSION = os.getenv("INFRAHUB_IMAGE_VER", None)
+CURRENT_DIRECTORY = Path(__file__).resolve()
+MAIN_DIRECTORY_PATH = Path(__file__).parent
 
 
 @task
@@ -47,6 +49,13 @@ def restart(context: Context, component: str = "") -> None:
 
     context.run("docker compose restart")
 
+@task
+def load_menu(ctx: Context) -> None:
+    """
+    Load schemas into InfraHub using infrahubctl.
+    """
+    ctx.run("infrahubctl menu load menus/", pty=True)
+
 
 @task
 def load_schema(ctx: Context) -> None:
@@ -65,19 +74,64 @@ def test(ctx: Context) -> None:
 
 
 @task(help={"override": "Redownload the compose file even if it already exists."})
-def download_compose_file(context: Context, override: bool = False) -> Path:  # noqa ARG001
+def download_compose_file(context: Context, override: bool = False) -> Path:  # noqa: ARG001
     """
     Download docker-compose.yml from InfraHub if missing or override is True.
     """
     compose_file = Path("./docker-compose.yml")
+    compose_url = os.getenv("INFRAHUB_COMPOSE_URL", "https://infrahub.opsmill.io")
 
     if compose_file.exists() and not override:
         return compose_file
 
-    response = httpx.get("https://infrahub.opsmill.io")
+    response = httpx.get(compose_url)
     response.raise_for_status()
 
-    with compose_file.open("w") as f:
+    with compose_file.open("w", encoding="utf-8") as f:
         f.write(response.content.decode())
 
     return compose_file
+
+@task(name="format")
+def format_python(ctx: Context) -> None:
+    """Run RUFF to format all Python files."""
+
+    exec_cmds = ["ruff format .", "ruff check . --fix"]
+    with ctx.cd(MAIN_DIRECTORY_PATH):
+        for cmd in exec_cmds:
+            ctx.run(cmd, pty=True)
+
+
+@task
+def lint_yaml(ctx: Context) -> None:
+    """Run Linter to check all Python files."""
+    print(" - Check code with yamllint")
+    exec_cmd = "yamllint ."
+    with ctx.cd(MAIN_DIRECTORY_PATH):
+        ctx.run(exec_cmd, pty=True)
+
+
+@task
+def lint_mypy(ctx: Context) -> None:
+    """Run Linter to check all Python files."""
+    print(" - Check code with mypy")
+    exec_cmd = "mypy --show-error-codes infrahub_sdk"
+    with ctx.cd(MAIN_DIRECTORY_PATH):
+        ctx.run(exec_cmd, pty=True)
+
+
+@task
+def lint_ruff(ctx: Context) -> None:
+    """Run Linter to check all Python files."""
+    print(" - Check code with ruff")
+    exec_cmd = "ruff check ."
+    with ctx.cd(MAIN_DIRECTORY_PATH):
+        ctx.run(exec_cmd, pty=True)
+
+
+@task(name="lint")
+def lint_all(ctx: Context) -> None:
+    """Run all linters."""
+    lint_yaml(ctx)
+    lint_ruff(ctx)
+    lint_mypy(ctx)
